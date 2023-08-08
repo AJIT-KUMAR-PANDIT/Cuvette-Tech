d99dd8cd0f5fde60cfb63a21b068c161a042304a
AJIT KUMAR PANDIT <49229210+AJIT-KUMAR-PANDIT@users.noreply.github.com>
2023-08-09 02:39:31 +0530

    Add files via upload

---
+++ Micro Project 4/resource/js/index.js
@@ -0,0 +1,55 @@
+// calculator
+
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine  = document.getElementById('9');
+const del  = document.getElementById('del');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const add = document.getElementById('addition');
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const add = document.getElementById('subtract');
+const dot = document.getElementById('dot');
+const zero = document.getElementById('0');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const reset = document.getElementById('reset');
+const equals = document.getElementById('equals');
+
+const addFunction = () => {
+    // Retrieve the values from input fields or variables
+    // Perform addition logic
+    // Display or store the result
+  };
+  
+  const subtractFunction = () => {
+    // Retrieve the values from input fields or variables
+    // Perform subtraction logic
+    // Display or store the result
+  };
+  
+  const multiplyFunction = () => {
+    // Retrieve the values from input fields or variables
+    // Perform multiplication logic
+    // Display or store the result
+  };
+  
+  const divideFunction = () => {
+    // Retrieve the values from input fields or variables
+    // Perform division logic
+    // Display or store the result
+  };
+  
+  const resetFunction = () => {
+    // Reset the calculator to initial state
+  };
+  
+  const equalsFunction = () => {
+    // Retrieve the values from input fields or variables
+    // Perform calculation based on the selected operator (+, -, *, /)
+    // Display or store the result
+  };
+  
