94eb4f83e76a6af0b665fddf256ecea0a588d581
AJIT KUMAR PANDIT <49229210+AJIT-KUMAR-PANDIT@users.noreply.github.com>
2023-08-10 02:27:59 +0530

    v3.0

--- Micro Project 4/resource/js/index.js
+++ Micro Project 4/resource/js/index.js
@@ -3,41 +3,27 @@
 let displayScreen = document.getElementById('displayScreen');
 let currentScreenValue = '';
 
-const addFunction = () => {
-    // Retrieve the values from input fields or variables
-    // Perform addition logic
-    // Display or store the result
-  };
-  
-  const subtractFunction = () => {
-    // Retrieve the values from input fields or variables
-    // Perform subtraction logic
-    // Display or store the result
-  };
-  
-  const multiplyFunction = () => {
-    // Retrieve the values from input fields or variables
-    // Perform multiplication logic
-    // Display or store the result
-  };
-  
-  const divideFunction = () => {
-    // Retrieve the values from input fields or variables
-    // Perform division logic
-    // Display or store the result
-  };
-  
-  const resetFunction = () => {
-    // Reset the calculator to initial state
-  };
   
   const equalsFunction = () => {
-    // Retrieve the values from input fields or variables
-    // Perform calculation based on the selected operator (+, -, *, /)
-    // Display or store the result
+    try{
+        currentScreenValue = eval(currentScreenValue);
+        displayScreen.value = currentScreenValue;
+    }catch(error){
+        displayScreen.value = "Error...";
+    }
   };
   
   function display(value){
     currentScreenValue += value;
-    displayScreen.value +=currentScreenValue;
+    displayScreen.value = currentScreenValue;
+  };
+
+  function delFunction(){
+    currentScreenValue = currentScreenValue.slice(0,-1);
+    displayScreen.value = currentScreenValue;
+  };
+
+  function resetFunction(){
+    currentScreenValue = '';
+    displayScreen.value = currentScreenValue;
   }
