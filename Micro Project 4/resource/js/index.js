b4dd3a490554c8dc9d9bf8b9b983a47a7a4161d3
AJIT KUMAR PANDIT <49229210+AJIT-KUMAR-PANDIT@users.noreply.github.com>
2023-08-09 17:12:30 +0530

    v2.0

--- Micro Project 4/resource/js/index.js
+++ Micro Project 4/resource/js/index.js
@@ -1,23 +1,7 @@
 // calculator
 
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine  = document.getElementById('9');
-const del  = document.getElementById('del');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six = document.getElementById('6');
-const add = document.getElementById('addition');
-const one = document.getElementById('1');
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const add = document.getElementById('subtract');
-const dot = document.getElementById('dot');
-const zero = document.getElementById('0');
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const reset = document.getElementById('reset');
-const equals = document.getElementById('equals');
+let displayScreen = document.getElementById('displayScreen');
+let currentScreenValue = '';
 
 const addFunction = () => {
     // Retrieve the values from input fields or variables
@@ -53,3 +37,7 @@ const addFunction = () => {
     // Display or store the result
   };
   
+  function display(value){
+    currentScreenValue += value;
+    displayScreen.value +=currentScreenValue;
+  }
